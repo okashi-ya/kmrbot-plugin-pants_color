d0f4bfd51644094e71000feff4d7b4c3f77d3ddc
古守のお菓子屋 <852496245@qq.com>
2023-08-18 17:43:36 +0800

    【miriya_pants_color】胖次颜色格式修改

--- pants_color.py
+++ pants_color.py
@@ -2,54 +2,73 @@ import enum
 
 
 class PantsType(enum.Enum):
+    PANTS_TYPE_SOLID = 0x000,   # 纯色
     PANTS_TYPE_FLOWER = 0x001,  # 花纹
 
 
 pants_color_data = {
     "红色": {
-        "colors": [0xFF0000]
+        "colors": [0xFF0000],
+        "type": PantsType.PANTS_TYPE_SOLID,
     },
     "绿色": {
-        "colors": [0x00FF00]
+        "colors": [0x00FF00],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "蓝色": {
-        "colors": [0x0000FF]
+        "colors": [0x0000FF],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "淡蓝色": {
-        "colors": [0x00FFFF]
+        "colors": [0x00FFFF],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "花纹淡蓝色": {
         "colors": [0x00FFFF],
-        "type": PantsType.PANTS_TYPE_FLOWER,
+        "type": [PantsType.PANTS_TYPE_FLOWER],
     },
     "水色": {
-        "colors": [0x00FFFF]
+        "colors": [0x00FFFF],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "紫色": {
-        "colors": [0xFF00FF]
+        "colors": [0xFF00FF],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "粉色": {
-        "colors": [0xFF80C0]
+        "colors": [0xFF80C0],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "黑色": {
-        "colors": [0x000000]
+        "colors": [0x000000],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "白色": {
-        "colors": [0xFFFFFF]
+        "colors": [0xFFFFFF],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "灰色": {
-        "colors": [0x646464]
+        "colors": [0x646464],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "金色": {
-        "colors": [0xFFE640]
+        "colors": [0xFFE640],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "薰衣草色": {
-        "colors": [0xB57EDC]
+        "colors": [0xB57EDC],
+        "type": [PantsType.PANTS_TYPE_SOLID],
     },
     "白色和粉色": {
-        "colors": [0xFFFFFF, 0xFF80C0]
+        "colors": [0xFFFFFF, 0xFF80C0],
+        "type": [PantsType.PANTS_TYPE_SOLID, PantsType.PANTS_TYPE_SOLID],
     },
     "白色和紫色": {
-        "colors": [0xFFFFFF, 0xFF00FF]
+        "colors": [0xFFFFFF, 0xFF00FF],
+        "type": [PantsType.PANTS_TYPE_SOLID, PantsType.PANTS_TYPE_SOLID],
+    },
+    "白色和紫色花紋": {
+        "colors": [0xFFFFFF, 0xFF00FF],
+        "type": [PantsType.PANTS_TYPE_SOLID, PantsType.PANTS_TYPE_FLOWER],
     }
 }
